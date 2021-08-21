0f904b3e72293eccb80bbc717f6e24bf78eb5594
Sunil <sunilnet4@gmail.com>
2021-08-22 00:20:21 +0530

    create: build-logic ex-1 complete

---
+++ operators-branching-loops/1-add-5-numbers.js
@@ -0,0 +1,12 @@
+const num1 = 5;
+const num2 = 13;
+const num3 = 7;
+const num4 = 21;
+const num5 = 48;
+
+function addition(a,b,c,d,e) {
+    const sum = a + b + c + d + e;
+    return sum;
+}
+
+console.log(addition(num1,num2,num3,num4,num5));
