c41b840bdbd73c10d0d941fc4a446a84171d85a2
Sunil <sunilnet4@gmail.com>
2021-08-22 00:39:00 +0530

    create: build-logic-ex-3 completes

---
+++ operators-branching-loops/3-max-min.js
@@ -0,0 +1,10 @@
+const num1 = 129;
+const num2 = 251; 
+
+if(num1 > num2) {
+    console.log("num1 is maximum")
+} else if(num1 < num2){
+    console.log("num2 is maximum")
+} else {
+    console.log("num1 and num2 are equal")
+}
