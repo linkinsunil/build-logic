2169165dbd6686480bdfe551529a3b79d19ce341
Sunil <sunilnet4@gmail.com>
2021-08-26 16:36:48 +0530

    6-build-logic-intermediate-done

---
+++ operators-branching-loops/intermediate/6-prime.js
@@ -0,0 +1,23 @@
+
+function isPrime(n) {
+
+    let flag = true;
+
+    for (let i = 2; i <= n / 2; i++) {
+
+        if (n % i == 0) { // 9 % 3 == 0 //false
+            flag = false;
+            break;
+        }
+
+    }
+   
+    if (flag == true)
+    console.log(n + " is prime");
+    else
+    console.log(n + " is not prime");
+
+}
+
+
+isPrime(9)
